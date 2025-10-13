b953b6970d9a38bda85e4983cb285f895bca8427
Vansh Nigam <vanshnigam16@gmail.com>
2025-10-13 21:53:04 +0530

    redis fix 2

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,7 +6,6 @@ dotenv.config();
 // It will automatically use the REDIS_URL from your environment variables in production.
 const redisClient = new IORedis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null, // This is important for BullMQ later
-  tls: {},
 });
 
 redisClient.on('connect', () => console.log('Redis client connected.'));
