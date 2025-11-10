07cf3f0f30bff6b250762ceff77805df7f05a94e
Vansh Nigam <vanshnigam16@gmail.com>
2025-11-11 01:54:59 +0530

    admin apis- reports, users, banners

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 // Create a new Redis client instance.
 // It will automatically use the REDIS_URL from your environment variables in production.
-const redisClient = new IORedis({
+const redisClient = new IORedis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null, // This is important for BullMQ later
 });
 
